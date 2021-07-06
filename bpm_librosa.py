f0b157e5d82156e2bef0b90ca188ae6148f2acf0
Arnav Gaur <60852260+arnav8@users.noreply.github.com>
2021-07-06 16:39:49 +0530

    Add files via upload

---
+++ bpm_librosa.py
@@ -0,0 +1,13 @@
+import librosa
+
+filename = "train3.wav"
+
+y, sr = librosa.load(filename)
+
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+print('Tempo', tempo)
+
+beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+
+#print(beat_times)
