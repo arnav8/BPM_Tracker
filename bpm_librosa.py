879f42a386ff4b85faccfdbd6fcaffd04260d1ac
Arnav Gaur <60852260+arnav8@users.noreply.github.com>
2021-07-24 23:41:50 +0530

    Update bpm_librosa.py

--- bpm_librosa.py
+++ bpm_librosa.py
@@ -4,6 +4,8 @@ filename = "train3.wav"
 
 #    Load the audio as a waveform `y`
 #    Store the sampling rate as `sr`
+#    loads and decodes the audio as a time series y, represented as a one-dimensional NumPy floating point array.
+#    The variable sr contains the sampling rate of y, that is, the number of samples per second of audio
 y, sr = librosa.load(filename)
 
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
