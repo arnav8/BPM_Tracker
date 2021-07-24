a0e07bbeb53c5da58d79ef7ee79f9d015020ae07
Arnav Gaur <60852260+arnav8@users.noreply.github.com>
2021-07-24 23:40:55 +0530

    Update bpm_librosa.py
    Added the meaning of variables,  y (waveform) and sr (sampling  rate)

--- bpm_librosa.py
+++ bpm_librosa.py
@@ -2,6 +2,8 @@ import librosa
 
 filename = "train3.wav"
 
+#    Load the audio as a waveform `y`
+#    Store the sampling rate as `sr`
 y, sr = librosa.load(filename)
 
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
